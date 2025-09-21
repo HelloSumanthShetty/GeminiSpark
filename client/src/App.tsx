eb199c9025a52476895f26eeb4d65642868e24ff
sumanth u shetty <sumanthshetty883@gmail.com>
2025-09-21 18:17:59 +0530

    frontend to client

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import SideBar from './components/SideBar'
+import { Routes,Route } from 'react-router-dom'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
+import ChatBot from './components/ChatBot'
+function App() {
+  
+
+  return (
+    <>
+    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+    <div className='flex min-h-screen w-screen '>
+      <SideBar />
+      <Routes>
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="/community" element={<Community />} />
+           
+      </Routes>
+      </div>
+    </div>
+    </> 
+  )
+}
+
+export default App
---
+++ client/src/components/ChatBot.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ChatBot = (props: Props) => {
+  return (
+    <div>ChatBot</div>
+  )
+}
+
+export default ChatBot
