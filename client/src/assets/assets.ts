11ec4e6dc0ffbfcd86f711d02d4bba10437f0397
sumanth u shetty <sumanthshetty883@gmail.com>
2025-10-12 20:44:06 +0530

    frontend and backend

--- client/src/assets/assets.ts
+++ client/src/assets/assets.ts
@@ -14,17 +14,6 @@ import logout_icon from "./logout_icon.svg";
 import diamond_icon from "./diamond_icon.svg";
 import gallery_icon from "./gallery_icon.svg";
 
-import ai_image1 from "./ai_image1.jpg";
-import ai_image2 from "./ai_image2.jpg";
-import ai_image3 from "./ai_image3.jpg";
-import ai_image4 from "./ai_image4.jpg";
-import ai_image5 from "./ai_image5.jpg";
-import ai_image6 from "./ai_image6.jpg";
-import ai_image7 from "./ai_image7.jpg";
-import ai_image8 from "./ai_image8.jpg";
-import ai_image9 from "./ai_image9.jpg";
-import ai_image10 from "./ai_image10.jpg";
-import ai_image11 from "./ai_image11.jpg";
 import Gemini_spark from "./Gemini_spark.svg"
 import Gemini_spark_dark from "./Gemini_spark_dark.svg"
 
